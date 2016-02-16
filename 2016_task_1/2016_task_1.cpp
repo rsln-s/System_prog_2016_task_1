d519f90ddfa9e09fd03c6f6d1cbe6c0dedc5a73a
Ruslan Shaydulin <ruslan@l2sh.ru>
2016-02-16 16:36:43 +0300

    Stage one done
    If not obvious, this is system programming course

---
+++ 2016_task_1/2016_task_1.cpp
@@ -0,0 +1,13 @@
+// 2016_task_1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int _tmain1(int argc, TCHAR *argv[]);
+
+int main(int argc, TCHAR *argv[])
+{
+	printf("hello\n");
+    return _tmain1(argc, argv);
+}
+
